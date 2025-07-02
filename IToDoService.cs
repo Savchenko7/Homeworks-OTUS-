f0eae30248c86638aab1032bfe83b426bf486e13
Savchenko7 <sanek777_1991@mail.ru>
2025-07-02 23:13:13 +0300

    Интерфесы ДЗ

---
+++ IToDoService.cs
@@ -0,0 +1,8 @@
+﻿public interface IToDoService
+{
+    IReadOnlyList<ToDoItem> GetAllByUserId(Guid userId);
+    IReadOnlyList<ToDoItem> GetActiveByUserId(Guid userId);
+    ToDoItem Add(ToDoUser user, string name);
+    void MarkCompleted(Guid id);
+    void Delete(Guid id);
+}
